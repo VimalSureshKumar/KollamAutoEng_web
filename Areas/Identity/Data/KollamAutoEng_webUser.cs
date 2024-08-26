f9707359c2bd6471d68d10f827993e366aa6a047
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2024-08-26 14:15:28 +1200

    Updated Front End CRUD Added User Fields

--- Areas/Identity/Data/KollamAutoEng_webUser.cs
+++ Areas/Identity/Data/KollamAutoEng_webUser.cs
@@ -3,24 +3,48 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using KollamAutoEng_web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.CodeAnalysis.Scripting;
 
 namespace KollamAutoEng_web.Areas.Identity.Data;
 
+public enum Gender
+{
+    Male, Female, Other, [Display(Name = "Prefer not to say")] Prefer_not_to_say
+}
+
 public class KollamAutoEng_webUser : IdentityUser
 {
+
     [Required(ErrorMessage = "Please enter First Name")]
     [MaxLength(25)]
     [RegularExpression("^[A-Za-z]+( [A-Za-z]+)*$", ErrorMessage = "Only letters and single spaces between words are allowed.")]
     [Display(Name = "First Name")]
     public string FirstName { get; set; }
+
     [Required(ErrorMessage = "Please enter Last Name")]
     [MaxLength(25)]
     [RegularExpression("^[A-Za-z]+( [A-Za-z]+)*$", ErrorMessage = "Only letters and single spaces between words are allowed.")]
     [Display(Name = "Last Name")]
     public string LastName { get; set; }
 
+    [Required(ErrorMessage = "Please enter an email address")]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Please enter a valid email address.")]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Please enter an phone number")]
+    [DataType(DataType.PhoneNumber)]
+    [MaxLength(17)]
+    [RegularExpression(@"^(\+64|0)\d{2,4}[\s-]?\d{4}[\s-]?\d{3,4}$|^(\+91|0)\d{10}$", ErrorMessage = "Please enter a valid phone number in New Zealand or India format.")]
+    [Display(Name = "Phone Number")]
+    public string PhoneNumber { get; set; }
+
+    [Display(Name = "Gender")]
+    public Gender? Gender { get; set; }
 }
 
 
---
+++ Migrations/20240826015210_usertables.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KollamAutoEng_web.Migrations
+{
+    /// <inheritdoc />
+    public partial class usertables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
